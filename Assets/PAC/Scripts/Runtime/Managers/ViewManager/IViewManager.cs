022f54c14004b99e7f43d6032cfff86bc3e5d319
IMorazel <emircelik@kalibagames.com>
2024-08-28 16:08:04 +0300

    feat: Implement ViewManager for managing UI views.

---
+++ Assets/PAC/Scripts/Runtime/Managers/ViewManager/IViewManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace PAC.Scripts.Runtime.Managers.LevelManager
+{
+    public class LoadViewParams<T> : EventArgs
+    {
+        public readonly string ViewName;
+        public Action<T> OnLoad;
+
+        readonly string _viewPresenterType;
+
+        public LoadViewParams(string viewName)
+        {
+            ViewName = viewName;
+            _viewPresenterType = typeof(T).Name;
+        }
+        
+        public string GetViewPresenterType()
+        {
+            return _viewPresenterType;
+        }
+    }
+    
+    public interface IViewManager
+    {
+        Action<string> OnViewLoaded { get; set; }
+        Action<string> OnViewClosed { get; set; }
+        void ForceAddToViewList<T>(LoadViewParams<T> viewPresenter, GameObject viewInstance);
+        void LoadView<T>(LoadViewParams<T> loadViewParams, bool closeAllViews = false, bool closeLastView = false);
+        void DestroyView<T>();
+        void DestroyAllViews();
+        bool IsAnyViewActive();
+        bool IsViewActive<T>();
+        bool IsCurrentView(string viewName);
+    }
+}
---
+++ Assets/PAC/Scripts/Runtime/Managers/ViewManager/ViewManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PAC.Scripts.Runtime.Managers.LevelManager;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using Object = UnityEngine.Object;
+
+namespace PAC.Scripts.Runtime.Managers.ViewManager
+{
+   public class ViewManager : IViewManager
+    {
+        readonly Dictionary<string, GameObject> _openedViews = new();
+
+        public Action<string> OnViewLoaded { get; set; }
+        public Action<string> OnViewClosed { get; set; }
+
+        public void ForceAddToViewList<T>(LoadViewParams<T> viewPresenter, GameObject viewInstance)
+        {
+            _openedViews[viewPresenter.GetViewPresenterType()] = viewInstance;
+        }
+
+        public void LoadView<T>(LoadViewParams<T> loadViewParams, bool closeAllViews = false, bool closeLastView = false)
+        {
+            if (closeAllViews)
+            {
+                DestroyAllViews();
+            }
+
+            if (closeLastView)
+            {
+                var lastView = _openedViews.Last();
+                GameObject viewInstance = _openedViews[lastView.Key];
+                Object.Destroy(viewInstance);
+                _openedViews.Remove(lastView.Key);
+                OnViewClosed?.Invoke(lastView.Key);
+            }
+            
+            if (!_openedViews.ContainsKey(loadViewParams.GetViewPresenterType()))
+            {
+                Addressables.LoadAssetAsync<GameObject>(loadViewParams.ViewName).Completed += (obj) =>
+                {
+                    if (obj.Status == AsyncOperationStatus.Succeeded)
+                    {
+                        GameObject viewPrefab = obj.Result;
+                        GameObject viewInstance = Object.Instantiate(viewPrefab);
+                        viewInstance.transform.SetAsLastSibling();
+                        viewInstance.GetComponent<Canvas>().sortingOrder = _openedViews.Count + 1;
+                        _openedViews[loadViewParams.GetViewPresenterType()] = viewInstance;
+                        loadViewParams.OnLoad?.Invoke(viewInstance.GetComponent<T>());
+                        OnViewLoaded?.Invoke(loadViewParams.GetViewPresenterType());
+                    }
+                };
+            }
+        }
+
+        public void DestroyView<T>()
+        {
+            var viewName = typeof(T).Name;
+            if (_openedViews.ContainsKey(viewName))
+            {
+                GameObject viewInstance = _openedViews[viewName];
+                Object.Destroy(viewInstance);
+                _openedViews.Remove(viewName);
+                OnViewClosed?.Invoke(viewName);
+            }
+        }
+
+        public void DestroyAllViews()
+        {
+            foreach (var view in _openedViews)
+            {
+                Object.Destroy(view.Value);
+            }
+            _openedViews.Clear();
+        }
+
+        public bool IsAnyViewActive()
+        {
+            return _openedViews.Count > 0;
+        }
+
+        public bool IsViewActive<T>()
+        {
+            return _openedViews.ContainsKey(typeof(T).Name);
+        }
+
+        public bool IsCurrentView(string viewName)
+        {
+            if (_openedViews.Count == 0)
+            {
+                return false;
+            }
+            
+            return _openedViews.Last().Key == viewName;
+        }
+    }
+}
