3e4415e9cca5198aacc86a0b5128d8c13e2b2c85
IMorazel <emircelik@kalibagames.com>
2024-08-28 16:19:31 +0300

    feat: Create BaseModel and BasePresenter classes for MVP architecture.

---
+++ Assets/PAC/Scripts/Runtime/Models/BaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PAC.Scripts.Runtime.Models
+{
+    public class BaseModel : IDisposable
+    {
+        public event Action OnDataChanged;
+        
+        protected void NotifyDataChanged() => OnDataChanged?.Invoke();
+        public virtual void Initialize(){}
+        public virtual void Clear() {}
+        public virtual void Dispose(){}
+    }
+}
