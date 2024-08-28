c3c5cfaf24ab2df6358d041c951216ae0267a14a
IMorazel <emircelik@kalibagames.com>
2024-08-28 14:35:41 +0300

    feat: Implement Service Locator pattern for dependency management.

---
+++ Assets/PAC/Scripts/Runtime/Launcher.cs
@@ -0,0 +1,16 @@
+﻿using PAC.Scripts.Runtime.ServiceLocator;
+using UnityEngine;
+
+namespace PAC.Scripts.Runtime
+{
+    public class Launcher : MonoBehaviour
+    {
+        private Locator _locator;
+        
+        private void Awake()
+        {
+            Debug.Log("Launcher Awake");
+            _locator = new Locator();
+        }
+    }
+}
