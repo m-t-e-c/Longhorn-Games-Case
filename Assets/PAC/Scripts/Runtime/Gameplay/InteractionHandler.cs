263b705251204413a2ec96c1abc20529e15e9d49
IMorazel <emircelik@kalibagames.com>
2024-08-28 14:42:02 +0300

    feat: Create InteractionHandler script for managing user interactions.

---
+++ Assets/PAC/Scripts/Runtime/Gameplay/InteractionHandler.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace PAC.Scripts.Runtime
+{
+    public class InteractionHandler : MonoBehaviour
+    {
+        [SerializeField] private LayerMask interactionLayer;
+        private Camera _camera;
+        
+        private SelectableObject _currentSelectedObject;
+        
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+        
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, interactionLayer))
+                {
+                    if (hit.collider.TryGetComponent(out SelectableObject selectableObject))
+                    {
+                        _currentSelectedObject?.Deselect();
+                        _currentSelectedObject = selectableObject;
+                        _currentSelectedObject.Select();
+                        return;
+                    }
+                    
+                    if (_currentSelectedObject != null && _currentSelectedObject.gameObject != hit.collider.gameObject)
+                    {
+                        var strategy = _currentSelectedObject.GetStrategyForMatch(hit.collider.gameObject);
+                        strategy?.Execute(_currentSelectedObject.transform, hit.transform);
+                    }
+                }
+            }
+        }
+    }
+}
