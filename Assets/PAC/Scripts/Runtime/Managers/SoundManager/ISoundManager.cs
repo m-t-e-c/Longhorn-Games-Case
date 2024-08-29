14d27d98f696a360113d769533316bebd05f071a
IMorazel <emircelik@kalibagames.com>
2024-08-29 21:23:39 +0300

    feat: Create SoundManager for managing audio.

---
+++ Assets/PAC/Scripts/Runtime/Managers/SoundManager/ISoundManager.cs
@@ -0,0 +1,10 @@
+﻿namespace PAC.Scripts.Runtime.Managers.SoundManager
+{
+    public interface ISoundManager
+    {
+        void PlaySound(string id);
+        void PlayMusic(string clipName);
+        void StopMusic();
+        void StopSound();
+    }
+}
---
+++ Assets/PAC/Scripts/Runtime/Managers/SoundManager/SoundManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PAC.Scripts.Runtime.Managers.SoundManager
+{
+
+    [Serializable]
+    public struct GameSound
+    {
+        public string id;
+        public AudioClip clip;
+    }
+
+    public class SoundManager : MonoBehaviour, ISoundManager
+    {
+        private AudioSource _soundEffectSource;
+        private AudioSource _musicSource;
+        
+        [SerializeField] private List<GameSound> gameSounds = new();
+
+        private void Awake()
+        {
+            _soundEffectSource = gameObject.AddComponent<AudioSource>();
+            _musicSource = gameObject.AddComponent<AudioSource>();
+            _musicSource.loop = true; 
+        }
+
+        public void PlaySound(string id)
+        {
+            var sound = gameSounds.Find(s => s.id == id);
+            if (sound.clip != null)
+            {
+                _soundEffectSource.PlayOneShot(sound.clip);
+            }
+        }
+
+        public void PlayMusic(string clipName)
+        {
+            var sound = gameSounds.Find(s => s.id == clipName);
+            if (sound.clip != null)
+            {
+                _musicSource.clip = sound.clip;
+                _musicSource.Play();
+            }
+        }
+
+        public void StopMusic()
+        {
+            _musicSource.Stop();
+        }
+
+        public void StopSound()
+        {
+            _soundEffectSource.Stop();
+        }
+    }
+}
