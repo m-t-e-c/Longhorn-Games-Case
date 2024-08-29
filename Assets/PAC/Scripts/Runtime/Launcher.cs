217505d5d3eb82db377d5a9af93031b0ebbaff5f
IMorazel <emircelik@kalibagames.com>
2024-08-29 20:43:03 +0300

    feat: Integrate IViewManager into Launcher.

--- Assets/PAC/Scripts/Runtime/Launcher.cs
+++ Assets/PAC/Scripts/Runtime/Launcher.cs
@@ -1,4 +1,6 @@
-﻿using PAC.Scripts.Runtime.ServiceLocator;
+﻿using PAC.Scripts.Runtime.Managers.LevelManager;
+using PAC.Scripts.Runtime.Managers.ViewManager;
+using PAC.Scripts.Runtime.ServiceLocator;
 using UnityEngine;
 
 namespace PAC.Scripts.Runtime
@@ -6,11 +8,15 @@ namespace PAC.Scripts.Runtime
     public class Launcher : MonoBehaviour
     {
         private Locator _locator;
+
+        private IViewManager _viewManager;
         
         private void Awake()
         {
-            Debug.Log("Launcher Awake");
             _locator = new Locator();
+
+            _viewManager = new ViewManager();
+            _locator.Register<IViewManager>(_viewManager);
         }
     }
 }
