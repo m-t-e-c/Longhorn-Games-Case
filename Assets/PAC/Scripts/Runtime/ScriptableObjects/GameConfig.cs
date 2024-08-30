3ed1e5e2d9479a39a0f2d733d73320d4eb686b0f
IMorazel <emircelik@kalibagames.com>
2024-08-30 05:42:44 +0300

    feat: Create GameConfig ScriptableObject for storing game configurations.

---
+++ Assets/PAC/Scripts/Runtime/ScriptableObjects/GameConfig.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace PAC.Scripts.Runtime.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "GameConfig", menuName = "PAC/Game Config", order = 0)]
+    public class GameConfig : ScriptableObject
+    {
+        [field:SerializeField] public bool IsSoundActive { get; private set; }
+        [field:SerializeField] public bool IsMusicActive { get; private set; }
+        
+        public void ToggleSound()
+        {
+            IsSoundActive = !IsSoundActive;
+        }
+        
+        public void ToggleMusic()
+        {
+            IsMusicActive = !IsMusicActive;
+        }
+    }
+}
