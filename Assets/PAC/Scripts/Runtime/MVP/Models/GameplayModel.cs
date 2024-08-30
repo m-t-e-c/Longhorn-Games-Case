075f51b91141054fa9cea21643261c90d2e8338e
IMorazel <emircelik@kalibagames.com>
2024-08-30 05:47:24 +0300

    feat: Create GameplayView UI and update GameplayModel.

---
+++ Assets/PAC/Scripts/Runtime/MVP/Models/GameplayModel.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using PAC.Scripts.Runtime.Level;
+using PAC.Scripts.Runtime.Managers.LevelConditionManager;
+using PAC.Scripts.Runtime.Managers.LevelManager;
+using PAC.Scripts.Runtime.ScriptableObjects;
+using PAC.Scripts.Runtime.ServiceLocator;
+
+namespace PAC.Scripts.Runtime.MVP.Models
+{
+    public class GameplayModel : BaseModel
+    {
+        public bool IsMusicActive { get; private set; }
+        public bool IsSoundActive { get; private set; }
+        public List<LevelCompletionCondition> LevelConditions { get; private set; }
+        public int Level { get; private set; }
+
+        private readonly GameConfig _gameConfig;
+        private readonly LevelConditionManager _levelConditionManager;
+        private readonly ILevelManager _levelManager;
+        
+        public GameplayModel()
+        {
+            _gameConfig = Locator.Instance.Get<GameConfig>();
+            _levelConditionManager = Locator.Instance.Get<LevelConditionManager>();
+            _levelManager = Locator.Instance.Get<ILevelManager>();
+            
+           GetSettingsDataFromConfig();
+           GetLevelData();
+        }
+
+        private void GetSettingsDataFromConfig()
+        {
+            IsMusicActive = _gameConfig.IsMusicActive;
+            IsSoundActive = _gameConfig.IsSoundActive;
+        }
+        
+        private void GetLevelData()
+        {
+            LevelConditions = _levelConditionManager.GetCompletionConditions();
+            Level = _levelManager.GetLevelIndex();
+        }
+        
+        public void SetMusicActive(bool isActive)
+        {
+            IsMusicActive = isActive;
+            NotifyDataChanged();
+        }
+        
+        public void SetSoundActive(bool isActive)
+        {
+            IsSoundActive = isActive;
+            NotifyDataChanged();
+        }
+    }
+}
