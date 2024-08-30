1fe2874cba08bff1e7781db242c0c38f95d433e1
IMorazel <emircelik@kalibagames.com>
2024-08-30 05:49:07 +0300

    feat: Register LevelManager, SoundManager, and GameConfig in Locator within Launcher.

--- Assets/PAC/Scripts/Runtime/Launcher.cs
+++ Assets/PAC/Scripts/Runtime/Launcher.cs
@@ -1,5 +1,8 @@
 ﻿using PAC.Scripts.Runtime.Managers.LevelManager;
+using PAC.Scripts.Runtime.Managers.SoundManager;
 using PAC.Scripts.Runtime.Managers.ViewManager;
+using PAC.Scripts.Runtime.MVP.Views;
+using PAC.Scripts.Runtime.ScriptableObjects;
 using PAC.Scripts.Runtime.ServiceLocator;
 using UnityEngine;
 
@@ -8,15 +11,37 @@ namespace PAC.Scripts.Runtime
     public class Launcher : MonoBehaviour
     {
         private Locator _locator;
+        
+        // Configs
+        [SerializeField] private GameConfig gameConfig;
 
+        // Mono Managers
+        [SerializeField] private SoundManager soundManager;
+        
+        // Custom Managers
         private IViewManager _viewManager;
+        private ILevelManager _levelManager;
         
         private void Awake()
         {
+            DontDestroyOnLoad(this);
+            
             _locator = new Locator();
+            
+            _locator.Register<GameConfig>(gameConfig);
 
             _viewManager = new ViewManager();
             _locator.Register<IViewManager>(_viewManager);
+            
+            _levelManager = new LevelManager();
+            _locator.Register<ILevelManager>(_levelManager);
+            
+            _locator.Register<ISoundManager>(soundManager);
+        }
+
+        private void Start()
+        {
+            _viewManager.LoadView(new LoadViewParams<MainMenuView>("MainMenuView"));
         }
     }
 }
